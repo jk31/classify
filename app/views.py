fdd97cf373e51740008d9a663289b75471597cb0
JK <jarekkornowicz@googlemail.com>
2020-08-22 23:38:30 +0200

    data columns and types in template

--- app/views.py
+++ app/views.py
@@ -10,11 +10,11 @@ def home(request):
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            print("form valid")
             df = pd.read_csv(request.FILES['data'])
-            print(df.info())
+            data_columns = dict(zip(df.columns, [str(x) for x in df.dtypes.values]))
+            return render(request, "app/home.html", {"form": form, "data_columns": data_columns})
         else:
-            print("form invalid")
+            print("###### form invalid #####")
     else:
         form = UploadFileForm()
     return render(request, "app/home.html", {"form": form})
