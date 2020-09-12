ff627438a1b83bddad38d6d6efc68590c3461a98
JK <jarekkornowicz@googlemail.com>
2020-09-12 19:56:36 +0200

    signals works

--- classify/settings.py
+++ classify/settings.py
@@ -45,7 +45,7 @@ EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
 # Application definition
 
 INSTALLED_APPS = [
-    "app",
+    "app.apps.AppConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,8 +131,8 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "mail.privateemail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = EMAIL_USERNAME
